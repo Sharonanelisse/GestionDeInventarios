99a6f2f3c5dce46a78e062da01a5ed476dcc66d4
Sharon <168245948+Sharonanelisse@users.noreply.github.com>
2025-09-27 22:43:11 -0600

    Actualizaciones
    Actualizacion Models, persistence, Tipo de movimiento etc

---
+++ src/main/java/com/smarroquin/gestiondeinventarios/models/Movimiento.java
@@ -0,0 +1,62 @@
+package com.smarroquin.gestiondeinventarios.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "movimientos")
+public class Movimiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "El tipo de movimiento es obligatorio")
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoMovimiento tipo;  // ENTRADA o SALIDA
+
+    @NotNull(message = "La cantidad es obligatoria")
+    @Min(value = 1, message = "La cantidad debe ser mayor a 0")
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    private String motivo;
+
+    @Column(nullable = false)
+    private LocalDateTime fecha = LocalDateTime.now();
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    public Movimiento() {}
+
+    public Movimiento(Long id, TipoMovimiento tipo, Integer cantidad, String motivo, LocalDateTime fecha) {
+        this.id = id;
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+        this.motivo = motivo;
+        this.fecha = fecha;
+    }
+
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
+
+    public TipoMovimiento getTipo() {return tipo;}
+    public void setTipo(TipoMovimiento tipo) {this.tipo = tipo;}
+
+    public Integer getCantidad() {return cantidad;}
+    public void setCantidad(Integer cantidad) {this.cantidad = cantidad;}
+
+    public String getMotivo() {return motivo;}
+    public void setMotivo(String motivo) {this.motivo = motivo;}
+
+    public LocalDateTime getFecha() {return fecha;}
+    public void setFecha(LocalDateTime fecha) {this.fecha = fecha;}
+
+    public Producto getProducto() {return producto;}
+    public void setProducto(Producto producto) {this.producto = producto;}
+}
